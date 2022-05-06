f8977785b5cec19155d9581f3fc4a833b7e9000f
manyyeon <udayeon@naver.com>
2022-05-06 21:05:56 +0900

    Process, Algorithm 클래스 만들기

---
+++ src/com/company/Algorithm.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.*;
+
+class ResultTime {
+    int total;
+    int avg;
+}
+
+class Algorithm {
+    ArrayList<Process> processList;
+    ResultTime waitingTime;
+    ResultTime turnaroundTime;
+}
