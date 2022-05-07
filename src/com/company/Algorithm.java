6cef1e247fad5eadf85e3233676759ca6ef721fb
manyyeon <udayeon@naver.com>
2022-05-07 15:21:53 +0900

    입력 받고 입력 받은 것들 출력해주기

--- src/com/company/Algorithm.java
+++ src/com/company/Algorithm.java
@@ -2,13 +2,16 @@ package com.company;
 
 import java.util.*;
 
+// 알고리즘 결과 시간
 class ResultTime {
-    int total;
-    int avg;
+    int total = 0;
+    int avg = 0;
 }
 
+// 알고리즘 클래스
 class Algorithm {
-    ArrayList<Process> processList;
-    ResultTime waitingTime;
-    ResultTime turnaroundTime;
+    ArrayList<Process> processList; // process들
+    ResultTime waitingTime; // 대기시간
+    ResultTime turnaroundTime; // 총처리시간
+    int timeQuantum; // time quantum
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,60 @@
 package com.company;
 
-public class Main {
+import java.util.*;
 
+public class Main {
+    int processNum; // process 개수
+    Algorithm algorithm; // 알고리즘
+
+    // 초기 프로세스들 출력
+    public void printInitialAllProcess() {
+        Iterator<Process> it = algorithm.processList.iterator();
+        System.out.println("-----------------------------------------");
+        System.out.println("time quantum : " + algorithm.timeQuantum);
+        System.out.println("\tarrival time\tburst time\tpriority");
+        while(it.hasNext()){
+            Process tmpProcess = it.next();
+            System.out.println("P" + tmpProcess.id + "\t\t\t" + tmpProcess.arrivalTime + "\t\t\t" + tmpProcess.burstTime + "\t\t\t" + tmpProcess.priority);
+        }
+        System.out.println("-----------------------------------------");
+    }
+
+    // 입력
+    public void input(){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.print("process 개수 입력 >> ");
+            processNum = sc.nextInt();
+            if(processNum >= 4) {
+                break;
+            } else {
+                System.out.println("프로세스 개수는 최소 4개 이상입니다. 다시 입력하세요.");
+            }
+        }
+
+        algorithm.processList = new ArrayList<Process>(); // process들을 담는 리스트
+        System.out.print("time quantum 입력 >> ");
+        algorithm.timeQuantum = sc.nextInt();
+
+        for(int i=0; i<processNum; i++){
+            Process newProcess = new Process();
+            newProcess.id = i+1;
+            System.out.println("프로세스 P" + newProcess.id + " 생성");
+            System.out.print("P" + newProcess.id + "의 arrival time : ");
+            newProcess.arrivalTime = sc.nextInt();
+            System.out.print("P" + newProcess.id + "의 burst time : ");
+            newProcess.burstTime = sc.nextInt();
+            System.out.print("P" + newProcess.id + "의 priority : ");
+            newProcess.priority = sc.nextInt();
+
+            algorithm.processList.add(newProcess);
+        }
+    }
     public static void main(String[] args) {
-	// write your code here
+	    Main mainClass = new Main();
+        mainClass.algorithm = new Algorithm();
+
+        mainClass.input();
+        mainClass.printInitialAllProcess();
     }
 }
